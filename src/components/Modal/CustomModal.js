065201bb87a39c281ada78cd2b1cbf5bc5a9e051
Angelique15 <mar.angel.go15@gmail.com>
2023-09-15 18:51:32 -0500

    modal-upgrade

---
+++ src/components/Modal/CustomModal.js
@@ -0,0 +1,76 @@
+// components/Modal/CustomModal.js
+import React from 'react';
+import Modal from 'react-modal';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/selectors';
+import { capitalizeFirstLetter } from 'helpers/capitalizeFirstLetter';
+import { routes } from 'helpers/constants';
+
+import {
+  LinkStyled,
+  ModalWrapper,
+  ModalContent,
+  CloseButton,
+  StartLosingWeightButton,
+  customModalStyle
+} from './ModalStyled';
+
+Modal.setAppElement('#root');
+
+export const DailyCalorieIntake = ({
+  isOpen,
+  onRequestClose,
+  dataForModal: { countedCalories, notAllowedFoodCategories },
+  closeModal,
+}) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const redirectTo = isLoggedIn ? routes.diaryToday : routes.signup;
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Resultado de Calorías"
+      style={customModalStyle}
+    >
+      <ModalWrapper>
+        <h2>Tu ingesta diaria recomendada de calorías es</h2>
+        <ModalContent>
+          <p style={{ fontFamily: 'Verdana', fontWeight: '700', fontSize: '48px', color: '#264061' }}>
+            {countedCalories} <span style={{ fontSize: '24px' }}>kcal</span>
+          </p>
+          <div>
+            <h3 style={{ color: '#264061' }}>Alimentos que no deberías comer</h3>
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+              {notAllowedFoodCategories.slice(0, 5).map(categorie => {
+                return (
+                  <li
+                    key={categorie}
+                    style={{
+                      backgroundColor: '#f7f7f7',
+                      border: '1px solid #ddd',
+                      borderRadius: '4px',
+                      margin: '4px 0',
+                      padding: '8px',
+                    }}
+                  >
+                    {capitalizeFirstLetter(categorie)}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </ModalContent>
+        <CloseButton onClick={onRequestClose}>X</CloseButton>
+        <LinkStyled to={redirectTo}>
+          <StartLosingWeightButton onClick={closeModal}>Comienza a perder peso</StartLosingWeightButton>
+        </LinkStyled>
+      </ModalWrapper>
+    </Modal>
+  );
+};
+
+export default DailyCalorieIntake;
+
+
+
--- src/components/Modal/ModalStyled.js
+++ src/components/Modal/ModalStyled.js
@@ -1,3 +1,4 @@
+// components/Modal/ModalStyled.js
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -104,4 +105,99 @@ export const ButtonStyled = styled.button`
   letter-spacing: 0.04em;
   color: ${props => props.theme.colors.white};
   cursor: pointer;
+`;
+//return styles
+
+export const customModalStyle = {
+  content: {
+    width: '90%', // Cambiar el ancho para tabletas y teléfonos móviles
+    maxWidth: '672px', // Ancho máximo para mantener una buena legibilidad
+    margin: 'auto',
+    border: '1px solid #E0E0E0',
+    '@media (max-width: 768px)': {
+      padding: '20px', // Cambiar el relleno para tabletas
+    },
+    '@media (max-width: 576px)': {
+      padding: '10px', // Cambiar el relleno para teléfonos móviles
+    },
+  },
+};
+
+export const ModalWrapper = styled.div`
+  text-align: center;
+  padding: 40px;
+  position: relative;
+
+  @media (max-width: 768px) {
+    padding: 20px; // Ajustar el relleno para tabletas
+  }
+
+  @media (max-width: 576px) {
+    padding: 10px; // Ajustar el relleno para teléfonos móviles
+  }
+`;
+
+export const ModalContent = styled.div`
+  margin: 20px;
+
+  @media (max-width: 768px) {
+    margin: 10px;
+  }
+
+  @media (max-width: 576px) {
+    margin: 5px;
+  }
+`;
+
+export const CloseButton = styled.button`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+
+  @media (max-width: 768px) {
+    top: 5px;
+    right: 5px;
+    font-size: 14px;
+  }
+
+  @media (max-width: 576px) {
+    top: 2px;
+    right: 2px;
+    font-size: 12px;
+  }
+`;
+
+export const StartLosingWeightButton = styled.button`
+  background-color: #FC842D;
+  width: 210px;
+  height: 43px;
+  border: none;
+  border-radius: 30px;
+  color: white;
+  font-family: Verdana, sans-serif;
+  font-weight: 700;
+  font-size: 14px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #E0601A;
+  }
+
+  @media (max-width: 768px) {
+    width: 160px;
+    height: 35px;
+    font-size: 12px;
+  }
+
+  @media (max-width: 576px) {
+    width: 120px;
+    height: 30px;
+    font-size: 10px;
+  }
 `;
