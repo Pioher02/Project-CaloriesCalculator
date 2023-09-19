b79e6c0127c36833a7410c9caae2bb954ba2ecf3
Pioher02 <dpaola.hernandezr@gmail.com>
2023-09-18 21:35:44 -0500

    Fix details

--- src/components/DailyCaloriesForm/DailyCaloriesForm.js
+++ src/components/DailyCaloriesForm/DailyCaloriesForm.js
@@ -115,12 +115,12 @@ export const DailyCaloriesForm = () => {
         <ColumnWrap>
           <Column>
             <Label>
-              Altura *
+              Altura,cm *
               <InputForm
                 value={heightValue}
                 type="number"
                 {...register('height', {
-                  required: 'Por favor ingresa tu altura ',
+                  required: 'Por favor ingresa tu altura en cm ',
                   min: {
                     value: 100,
                     message: 'Altura mínima 100 cm.',
@@ -153,12 +153,12 @@ export const DailyCaloriesForm = () => {
               {errors?.age && <Error>{errors?.age?.message}</Error>}
             </Label>
             <Label>
-              Peso actual *
+              Peso actual, kg *
               <InputForm
                 value={currentWeightValue}
                 type="number"
                 {...register('currentWeight', {
-                  required: 'Ingrese su nombre de usuario',
+                  required: 'Ingrese su peso actual en kg',
                   min: {
                     value: 20,
                     message: 'Peso mínimo 20 kg',
@@ -177,12 +177,12 @@ export const DailyCaloriesForm = () => {
 
           <Column>
             <Label>
-              Peso deseado *
+              Peso deseado, kg *
               <InputForm
                 value={desiredWeightValue}
                 type="number"
                 {...register('desiredWeight', {
-                  required: 'Por favor ingrese su peso deseado',
+                  required: 'Por favor ingrese su peso deseado en kg',
                   min: {
                     value: 20,
                     message: 'Peso mínimo 20 kg',
