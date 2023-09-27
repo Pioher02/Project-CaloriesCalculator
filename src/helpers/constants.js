c17ff9b75d40bb51d4832218ce0db2c848b5e8a9
Pioher02 <dpaola.hernandezr@gmail.com>
2023-09-27 15:22:08 -0500

    Rutas para desarrollo y producción

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,8 +4,7 @@ export const initialDate = moment(new Date()).format('DD-MM-YYYY');
 
 export const bloodTypes = [1, 2, 3, 4];
 
-export const baseURL = "http://localhost:3001/api"
-//https://backend-calories-calculator.vercel.app  
+export const baseURL = process.env.NODE_ENV == "development" ? "http://localhost:3001/api" : "https://backend-calories-calculator.vercel.app"
 
 export const routes = {
   main: '/',
